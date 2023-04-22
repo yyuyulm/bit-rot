3df234dfa32ab071157c2faf4405d3b1cd3e3e0b
yyuyulm <gk2345@nyu.edu>
2023-04-22 18:05:13 -0400

    init

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let bits = [];
+let drawDimention = 80;
+let displayDimention = 500;
+
+let img;
+
+function drawBits(img){
+  img.loadPixels();
+  for(i=0; i< img.pixels.length;i+=4){
+    img.pixels[i]=random(0,255);
+    img.pixels[i+1]=random(0,255);
+    img.pixels[i+2]=random(0,255);
+    img.pixels[i+3]=255;
+  }
+  img.updatePixels();
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(displayDimention, displayDimention)
+  background(255);
+  noSmooth();
+  noLoop();
+  
+  img = createImage(drawDimention,drawDimention);
+  drawBits(img);
 }
 
 function draw() {
-  // put drawing code here
+  image(img,0,0,displayDimention,displayDimention, 0.25, 0.25, 79.5, 79.5);
 }
