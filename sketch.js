a42bdfe1bbabcab5ce0ca5952d0bb7bf30452f18
yyuyulm <gk2345@nyu.edu>
2023-04-27 10:29:50 -0400

    webpage styling

--- sketch.js
+++ sketch.js
@@ -1,30 +1,95 @@
-let bits = [];
 let drawDimention = 80;
-let displayDimention = 500;
+let seed = 10;
+let startDate = new Date(1968,12,9,12);
+let size = 0.6;
 
+let bits = [];
+let ints = [];
+let cnv;
 let img;
+let displayDimention = 500;
+let currentDate;
+let rotIteration = 0;
+let currentSeed;
+let pixelCount = drawDimention**2;
+let intCount = pixelCount*3;
+let bitCount = intCount*8;
 
-function drawBits(img){
-  img.loadPixels();
-  for(i=0; i< img.pixels.length;i+=4){
-    img.pixels[i]=random(0,255);
-    img.pixels[i+1]=random(0,255);
-    img.pixels[i+2]=random(0,255);
-    img.pixels[i+3]=255;
+function iniBits(){
+  for(let i=0; i < bitCount; i++){
+    bits[i]=0;
+  }
+}
+
+function myRand(co = createVector (0,0)){
+  let result = fract(sin(co.dot(createVector(12.9898, 78.233))) * 43758.5453);
+  return result;
+}
+
+function getRotInteration(){
+  currentDate = new Date(year(),month(),day(),hour(),minute(),second(),millis());
+  let mili = currentDate.getTime()-startDate.getTime();
+  rotIteration = mili/86400000;
+}
+
+function rotBit(){
+  let rand = myRand(createVector (currentSeed,0))*bitCount;
+  let randInt = Math.floor(rand);
+  bits[randInt] = 1-bits[randInt];
+  currentSeed = rand;
+}
+
+function bitToInt(bit8){
+  let val = 0;
+  for(let i=0; i < 8;i++){
+    val += bit8[i] * 2**(7-i);
+  }
+  return val;
+}
+
+function bitsToInts(){
+  for(let i=0; i < intCount; i++){
+    ints[i] = bitToInt(bits.slice(i*8,i*8+8));
+  }
+}
+
+function drawBits(){
+  for(let i=0; i< pixelCount;i++){
+    img.pixels[i*4] = ints[i*3];
+    img.pixels[i*4+1] = ints[i*3+1];
+    img.pixels[i*4+2] = ints[i*3+2];
+    img.pixels[i*4+3]=255;
   }
   img.updatePixels();
 }
 
 function setup() {
-  createCanvas(displayDimention, displayDimention)
-  background(255);
+  displayDimention = size * min(windowWidth,windowHeight);
+  cnv = createCanvas(displayDimention, displayDimention);
+  cnv.id('sketch');
+
   noSmooth();
-  noLoop();
+  frameRate(1);
+  //noLoop();
+  pixelDensity(1);
   
   img = createImage(drawDimention,drawDimention);
-  drawBits(img);
+  img.loadPixels();
 }
 
 function draw() {
+  iniBits();
+  getRotInteration();
+  currentSeed = seed;
+  for(let i=0; i<rotIteration; i++){
+    rotBit();
+  }
+  bitsToInts();
+  drawBits();
   image(img,0,0,displayDimention,displayDimention, 0.25, 0.25, 79.5, 79.5);
+}
+
+function windowResized() {
+  displayDimention = size * min(windowWidth,windowHeight);
+  resizeCanvas(displayDimention, displayDimention);
 }
